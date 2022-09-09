9b38bb263433aaf2f6e58f6328954c38dd32b19f
Squashedapricot <113000518+Squashedapricot@users.noreply.github.com>
2022-09-09 16:52:23 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin//bash
+
+echo Installing brew.......
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo Installing apps.......
+
+brew install git sublime-text fish kitty vscodium neofetch
+
+echo Installing python3.....
+
+brew install python3
+
+echo Installing pip3.....
+
+python3 get-pip.py
+
+echo And,we are done.
+
+echo Congratulations, Now you are a Hacker!
